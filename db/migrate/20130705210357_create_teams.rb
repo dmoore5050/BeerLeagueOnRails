a5aab1188a6ff08df9e48349079a500a3a6eebbe
dmoore5050 <dustinmoore@gmail.com>
2013-07-05 16:13:35 -0500

    generated team model/controller

---
+++ db/migrate/20130705210357_create_teams.rb
@@ -0,0 +1,13 @@
+class CreateTeams < ActiveRecord::Migration
+  def change
+    create_table :teams do |t|
+      t.string :teamname
+      t.string :owner_first_name
+      t.string :owner_last_name
+      t.integer :zip_code
+      t.string :phone_number
+      t.string :sponsor
+      t.timestamps
+    end
+  end
+end

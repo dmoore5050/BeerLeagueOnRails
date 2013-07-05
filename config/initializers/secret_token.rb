8a4be3d9b1e8df35c85c1f1953d1cbc4f395153c
dmoore5050 <dustinmoore@gmail.com>
2013-07-05 16:03:24 -0500

    rails new/postgres/no test unit/devise/cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beerleague::Application.config.secret_token = '5347791fd92a4c04a9a09a5c9fb8f8a7a9c8823fc70ef0d2aa5bfab3d5f5b50e01d714c2211cd96b8cfaa4680ea494c1961f9e3ea3d61c3a75c0a4967140abc7'
